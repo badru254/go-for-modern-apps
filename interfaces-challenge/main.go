77c0f514dc6a01a42b1d33c3b0153a7791097f93
Badru254 <suleybadr@yahoo.com>
2022-05-12 02:44:49 +0300

    Interface challenge

--- interfaces-challenge/main.go
+++ interfaces-challenge/main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
+
+type Capper struct {
+	Msg io.Writer
+}
+
+func (c *Capper) Write(data []byte) (int, error) {
+	//Option A
+	diff := byte('a' - 'A')
+	out := make([]byte, len(data))
+
+	for i, c := range data {
+		if c >= 'a' && c <= 'z' {
+			c -= diff
+		}
+
+		out[i] = c
+
+	}
+
+	//Option A output
+	return c.Msg.Write(out)
+
+	// //Option B
+	// str := string(data)
+
+	// strUpper := strings.ToUpper(str)
+
+	// //Option B output
+	// return c.Msg.Write([]byte(strUpper))
+
+}
 
 func main() {
 
-	fmt.Println("Default start of file")
+	c := &Capper{os.Stdout}
+	fmt.Fprintln(c, "Hello there")
 
 }
